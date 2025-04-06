bb9507d2f632c7cc81647944945a03fb2bf26847
ingdantev <ingdantev@gmail.com>
2025-04-06 05:36:15 +0000

    fix all

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,67 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as fs from 'fs'
+import * as path from 'path';
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
+var ffmpeg = require('fluent-ffmpeg')
+
+  ffmpeg.setFfmpegPath(ffmpegPath)
+  import { Buffer } from "buffer";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("myogg1")
   getHello(): string {
+    
     return this.appService.getHello();
   }
+  @Post('myogg')
+  async agregaRegistro(@Body() cuerpo:any){
+    const res = cuerpo.base64 
+    
+    //base64 to oga
+    const base = fixPathAudio("filteName.oga")
+    fs.writeFileSync(base ,res,'base64'); 
+    
+    //oga to mp3
+      const inputOgxFile = path.join(base); // Reemplaza 'audio.ogx' con la ruta real de tu archivo .ogx
+      const audiomp3 = fixPathAudio('audio.mp3')
+      const outputMp3File = path.join(audiomp3)
+      try {
+        await convertOgxToMp3(inputOgxFile, outputMp3File);
+        console.log(`Archivo convertido guardado en: ${outputMp3File}`);
+      } catch (error) {
+        console.error('La conversión falló:', error);
+      }
+    //mp3 to base64
+    
+    const base64String = audiomp3;
+    const contents = fs.readFileSync(base64String, {encoding: 'base64'});
+
+    return {"bybase64":contents}
+  
+  }
+}
+
+async function convertOgxToMp3(inputFilePath: string, outputFilePath: string): Promise<void> {
+  var outStream = fs.createWriteStream(outputFilePath);
+
+ffmpeg()
+.input(inputFilePath)
+.audioQuality(96)
+.toFormat("mp3")
+.on('error', error => console.log(`Encoding Error: ${error.message}`))
+.on('exit', () => console.log('Audio recorder exited'))
+.on('close', () => console.log('Audio recorder closed'))
+.on('end', () => console.log('Audio Transcoding succeeded !'))
+.pipe(outStream, { end: true });
+  
+}
+
+
+export const fixPathAudio = (recursoAssets:string)=>{
+   console.log(`Starting directory: ${process.cwd()}`);
+   return `${path.join(process.cwd(),'/','dist/src/',recursoAssets)}`
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configLoader } from 'config-loader';
+import { envSchema } from 'env-schema';
+import { HttpModule } from '@nestjs/axios';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load:[configLoader],
+      validationSchema:envSchema
+    }),
+    ConfigModule.forRoot(),
+    HttpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
